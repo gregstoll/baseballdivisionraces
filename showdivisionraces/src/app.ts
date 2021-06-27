0ee661f1704d7bb3c20894815967dc8b300d13e2
gregstoll <greg@gregstoll.com>
2021-06-26 21:49:26 -0500

    install plotly, showing simple graph

--- showdivisionraces/src/app.ts
+++ showdivisionraces/src/app.ts
@@ -1,5 +1,12 @@
+import Plotly from 'plotly.js-dist-min';
+
 (async function() {
     let response = await fetch('data/2019.json');
     let data2019 : any = await response.json();
-    alert(data2019.metadata[200].name);
+    //alert(data2019.metadata[200].name);
+    const divChart = document.getElementById("divisionchart");
+    Plotly.newPlot( divChart, [{
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 8, 16] }], {
+        margin: { t: 0 } } );
 })();
