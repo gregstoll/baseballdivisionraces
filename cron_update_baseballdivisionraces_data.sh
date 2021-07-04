be224d7280ba0c57ed001da0d1ab96cb8428cb91
Greg Stoll <greg@gregstoll.com>
2021-07-04 11:22:13 -0500

    make cron script a little more robust

--- cron_update_baseballdivisionraces_data.sh
+++ cron_update_baseballdivisionraces_data.sh
@@ -3,4 +3,11 @@
 cd /home/gregstoll/projects/baseballdivisionraces.git
 . .venv/bin/activate
 .venv/bin/python3 getmlbstandings/getmlbstandings.py -u
-cp getmlbstandings/data/*.json showdivisionraces/dist/data
+oldfilesize=$(wc -c <"showdivisionraces/dist/data/$(date +%Y).json")
+newfilesize=$(wc -c <"getmlbstandings/data/$(date +%Y).json")
+echo "old file size $oldfilesize"
+echo "new file size $newfilesize"
+if [ $newfilesize -ge $oldfilesize ]; then
+    echo "copying"
+    cp getmlbstandings/data/$(date +%Y).json showdivisionraces/dist/data
+fi
