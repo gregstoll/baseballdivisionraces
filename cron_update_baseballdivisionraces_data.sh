4f1f2a0df8bf73cd961d7b73d58e1e97988048da
Greg Stoll <greg@gregstoll.com>
2021-07-04 20:43:16 -0500

    fix cron script

--- cron_update_baseballdivisionraces_data.sh
+++ cron_update_baseballdivisionraces_data.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-cd /home/gregstoll/projects/baseballdivisionraces.git
+pushd /home/gregstoll/projects/baseballdivisionraces.git > /dev/null
 . .venv/bin/activate
 .venv/bin/python3 getmlbstandings/getmlbstandings.py -u
 oldfilesize=$(wc -c <"showdivisionraces/dist/data/$(date +%Y).json")
 newfilesize=$(wc -c <"getmlbstandings/data/$(date +%Y).json")
-echo "old file size $oldfilesize"
-echo "new file size $newfilesize"
+#echo "old file size $oldfilesize"
+#echo "new file size $newfilesize"
 if [ $newfilesize -ge $oldfilesize ]; then
-    echo "copying"
+    #echo "copying"
     cp getmlbstandings/data/$(date +%Y).json showdivisionraces/dist/data
 fi
+popd > /dev/null
